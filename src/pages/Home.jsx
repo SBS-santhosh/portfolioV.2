585e2841f83c8a6f1344cc41b809dea4384b2a0e
NotSanthosH <notsanthosh@NotSanthosHs-MacBook-Air.local>
2025-10-21 21:51:31 +0200

    still in dev

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import { ThemeToggle } from "../components/ThemeToggle"
+import { StarBackgroud } from "../components/StarBackgroud" 
+
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-backgroud text-foregroud overflow-x-hidden">
+        
+{/* theme toggle */}
+<ThemeToggle />
+
+
+{/* Backgroud effect */}
+<StarBackgroud/>
+
+
+
+
+
+{/* navigation bar  */}
+
+{/* main section */}
+
+
+
+
+{/* Footer  */}
+
+        </div>
+    )
+}
