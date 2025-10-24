3da770222f0f6c423bd6387a7aac09f27554966e
NotSanthosH <notsanthosh@NotSanthosHs-MacBook-Air.local>
2025-10-24 18:12:42 +0200

    M. Pierron Vialard -Projets professionnels

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -41,7 +41,7 @@ export const ProjectsSection = () => {
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           {" "}
-         Projet <span className="text-primary"> Professionnel </span>
+         Projet <span className="text-primary"> professionnels </span>
         </h2>
 
         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
