5f7be7bf84d7f8128a2c692d96da86061031bee7
NotSanthosH <notsanthosh@NotSanthosHs-MacBook-Air.local>
2026-03-01 00:25:00 +0100

    update cv

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
 
               {/* Bouton de téléchargement du CV */}
               <a
-                href="/CV_stage_2025.pdf"
+                href="/santhosh_cv.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
